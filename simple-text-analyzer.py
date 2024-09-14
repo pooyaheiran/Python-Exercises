5c117fdf1deff495c422051e6b77cd8c9440dc02
pooyaheiran <159570375+pooyaheiran@users.noreply.github.com>
2024-09-14 11:31:20 +0330

    Update simple-text-analyzer.py

--- simple-text-analyzer.py
+++ simple-text-analyzer.py
@@ -4,7 +4,7 @@ import tkinter as tk
 win = tk.Tk()
 win.title('simple-text-analyzer')
 win.resizable(0,0)
-win.configure(bg="black")
+win.configure(bg="#EDF4F2")
 
 #count word in sentence
 def word(sentence):
@@ -12,7 +12,7 @@ def word(sentence):
     return word_count
 
 
-#count vowel in sentence
+#count vowel in a sentence
 def vowel(sentence):
     vowel_count = 0
     vowel_list = ["a",'e',"i","o","u","A","E","I","O","U"]
@@ -22,7 +22,7 @@ def vowel(sentence):
     return vowel_count
 
 
-#count letters in sentence
+#count letters in a sentence
 def letter(letters):
     letter_count = 0
     for i in letters:
@@ -34,18 +34,45 @@ def letter(letters):
 
 def show():
     show_word = word(entry.get())
-    show_vowel = vowel(entry.get())
     show_letter = letter(entry.get())
-    lbl.config(text=f"word: {show_word} \n\nletter: {show_letter}\n\nvowel: {show_vowel}")
+    show_vowel = vowel(entry.get())
+    
+    lbl.config(text=f"word: {show_word}\n\nletter: {show_letter}\n\nvowel: {show_vowel}")
     lbl.after(100,show)
 
 
-entry = tk.Entry(win,font=("",15),width=100,background="black",foreground="#AAFF00")
-lbl = tk.Label(win,text='',font=("",15,"bold"),foreground="#AAFF00",background="black")
 
+entry_text = tk.Label(
+    
+    win,text="enter your text",
+    font=("Fira Code",25),background="#EDF4F2",
+    foreground="#31473A"
+
+)
+
+entry = tk.Entry(
+    
+    win,font=("Fira Code",15),
+    width=100,background="#EDF4F2",
+    foreground="#31473A"
+    
+)
+
+
+lbl = tk.Label(
+    
+    win,text='',
+    font=("Fira Code",25),
+    foreground="#31473A",
+    background="#EDF4F2"
+
+)
+
+entry_text.pack()
 entry.pack(side="top")
-lbl.pack(side="left")
+lbl.pack(pady="50")
+
 
 show()
-win.mainloop()
 
+win.mainloop()
